3493e7828f4ae14c2a3cce1e654d352fcfcd2c7e
JontyBurden <azttorrs@gmail.com>
2021-07-20 13:27:23 +1200

    tested web scraping on trademe and stored results in csv

--- src/post_scraper.py
+++ src/post_scraper.py
@@ -1,6 +1,6 @@
 from info import *
 from bs4 import BeautifulSoup
-import requests
+import requests, csv
 
 URL = 'https://www.trademe.co.nz/a/marketplace/computers/peripherals/keyboards/search?search_string=mechanical%20keyboards&condition=used'
 
@@ -8,23 +8,41 @@ source = requests.get(URL).text
 
 soup = BeautifulSoup(source, 'lxml')
 
-item = soup.find('div', class_='o-card')
+csv_file = open('trademeListings.csv', 'w', newline='')
 
-# link
-link = item.find('a', class_='tm-marketplace-search-card__detail-section')['href']
-# listing information
-item_info = item.find('div', class_='tm-marketplace-search-card__details ng-star-inserted')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['listing title', 'price', 'location', 'link'])
 
-# listing title
-title = item_info.find('div', class_='tm-marketplace-search-card__title')['aria-label']
+for item in soup.find_all('tg-col', class_='l-col l-col--has-flex-contents ng-star-inserted'):
 
-# listing price
-price = item_info.find('div', class_='tm-marketplace-search-card__footer-pricing-row')
-price = price.find('div', class_='tm-marketplace-search-card__price-left')['aria-label']
+    try:
 
+        # link
+        link = item.find('a', class_='tm-marketplace-search-card__detail-section')['href']
+        listing_link = f'https://www.trademe.co.nz/a/{link}'
 
-# listing location
-location = item_info.find('div', class_='tm-marketplace-search-card__location ng-star-inserted')['aria-label']
+        # listing title
+        title = item.find('div', class_='tm-marketplace-search-card__title')['aria-label']
 
-print(title, price, location, link)
+        # listing price
+        price = item.find('div', class_='tm-marketplace-search-card__footer-pricing-row')
+        price = price.find('div', class_='tm-marketplace-search-card__price-left')['aria-label']
 
+        # listing location
+        location = item.find('div', class_='tm-marketplace-search-card__location ng-star-inserted')['aria-label']
+        
+    except Exception as e:
+        title = None
+        price = None
+        location = None
+
+    print(title)
+    print(price)
+    print(location)
+    print(listing_link)
+
+    print()
+
+    csv_writer.writerow([title, price, location, listing_link])
+
+csv_file.close()
